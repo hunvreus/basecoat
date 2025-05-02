7c13727903ad4b7c2800515c0a293431675f1f7b
Ronan Berder <hunvreus@gmail.com>
2025-05-02 18:27:45 +0800

    Added sidebar

---
+++ src/js/sidebar.js
@@ -0,0 +1,27 @@
+window.basecoat = window.basecoat || {};
+window.basecoat.registerSidebar= function(Alpine) {
+  if (Alpine.components && Alpine.components.sidebar) return;
+
+  Alpine.data('sidebar', (initialOpen = true) => ({
+    id: null,
+    open: true,
+
+    init() {
+      this.id = this.$el.id;
+      this.open = initialOpen;
+    },
+
+    $main: {
+      '@sidebar:open.window'(e) { if (!this.id || (e.detail && e.detail.id === this.id)) this.open = true },
+      '@sidebar:close.window'(e) { if (!this.id || (e.detail && e.detail.id === this.id)) this.open = false },
+      '@sidebar:toggle.window'(e) {
+        if (!this.id || (e.detail && e.detail.id === this.id)) {
+          this.open = !this.open;
+          console.log('toggle', e);
+        }
+      },
+      ':aria-hidden'() { return !this.open },
+      ':inert'() { return !this.open },
+    },
+  }));
+};
