7749810bb1223122ea1ad18f59dbe899043c3b17
Ronan Berder <hunvreus@gmail.com>
2025-05-04 15:58:10 +0800

    Working on charts and sidebar

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -2,22 +2,24 @@ window.basecoat = window.basecoat || {};
 window.basecoat.registerSidebar= function(Alpine) {
   if (Alpine.components && Alpine.components.sidebar) return;
 
-  Alpine.data('sidebar', (initialOpen = true) => ({
-    id: null,
-    open: true,
+  Alpine.data('sidebar', (initialOpen = true, initialMobileOpen = false) => ({
+    open: window.innerWidth >= 768
+      ? initialOpen
+      : initialMobileOpen,
 
     init() {
-      this.id = this.$el.id;
-      this.open = initialOpen;
+      this.$nextTick(() => {
+        this.$el.removeAttribute('data-uninitialized');
+      });
     },
 
     $main: {
-      '@sidebar:open.window'(e) { if (!this.id || (e.detail && e.detail.id === this.id)) this.open = true },
-      '@sidebar:close.window'(e) { if (!this.id || (e.detail && e.detail.id === this.id)) this.open = false },
-      '@sidebar:toggle.window'(e) { if (!this.id || (e.detail && e.detail.id === this.id)) this.open = !this.open },
+      '@sidebar:open.window'(e) { this.open = true },
+      '@sidebar:close.window'(e) { this.open = false },
+      '@sidebar:toggle.window'(e) { this.open = !this.open },
       '@click'(e) { if (e.target === this.$el) this.open = false },
       ':aria-hidden'() { return !this.open },
-      ':inert'() { return !this.open },
+      ':inert'() { return !this.open }
     },
   }));
 };
