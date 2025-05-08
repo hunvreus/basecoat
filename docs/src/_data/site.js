9ec75a7fd76fbfb5e9febd5682bddf7173e54368
Ronan Berder <hunvreus@gmail.com>
2025-05-08 11:48:04 +0800

    Adding OG and twitter card, cleaning up images

---
+++ docs/src/_data/site.js
@@ -0,0 +1,9 @@
+export default {
+  title: "Basecoat",
+  description: "A components library built with Tailwind CSS that works with any web stack.",
+  author: {
+    name: "Ronan Berder",
+    x: "@hunvreus"
+  },
+  url: process.env.ELEVENTY_ENV === 'development' ? "http://localhost:8080" : "https://basecoatui.com"
+};
